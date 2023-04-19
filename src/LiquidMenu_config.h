47fba2a371561e782cf0d5fe55f5b2bab8bc1263
Sebastian Porombka <me@queaker.de>
2023-04-19 09:12:37 +0200

    changed configuration for gpsdo

--- src/LiquidMenu_config.h
+++ src/LiquidMenu_config.h
@@ -58,19 +58,19 @@ used in the library, also configures the debugging messages.
 
 
 /// Configures the number of available variables per line.
-const uint8_t MAX_VARIABLES = 5; ///< @note Default: 5
+const uint8_t MAX_VARIABLES = 2; ///< @note Default: 5
 
 /// Configures the number of available functions per line.
-const uint8_t MAX_FUNCTIONS = 8; ///< @note Default: 8
+const uint8_t MAX_FUNCTIONS = 1; ///< @note Default: 8
 
 /// Configures the number of available lines per screen.
-const uint8_t MAX_LINES = 12; ///< @note Default: 12
+const uint8_t MAX_LINES = 4; ///< @note Default: 12
 
 /// Configures the number of available screens per menu.
-const uint8_t MAX_SCREENS = 14; ///< @note Default: 14
+const uint8_t MAX_SCREENS = 1; ///< @note Default: 14
 
 /// Configures the number of available menus per menus system.
-const uint8_t MAX_MENUS = 8; ///< @note Default: 8
+const uint8_t MAX_MENUS = 2; ///< @note Default: 8
 
 /*!
  * Enable/disable hiding the focus indicator.
@@ -80,7 +80,7 @@ const uint8_t MAX_MENUS = 8; ///< @note Default: 8
  * indicator will go from the last focusable line directly to the first
  * focusable line without disappearing (i.e. it will be always visible).
  */
-#define LM_FOCUS_INDICATOR_GHOSTING true ///< @note Default: true
+#define LM_FOCUS_INDICATOR_GHOSTING false ///< @note Default: true
 
 // Turns the debugging messages on or off.
 #define LIQUIDMENU_DEBUG false ///< @note Default: false
